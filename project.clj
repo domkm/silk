4b5d8dca7c0d4136997750578acc8774a1bee2d6
Dom Kiva-Meyer <git@domkm.com>
2014-08-04 08:08:55 -0700

    add description and url to project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject com.domkm/silk "0.0.1-SNAPSHOT"
 
-  :description "FIXME: write description"
+  :description "Isomorphic Clojure & ClojureScript Routing"
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/domkm/silk"
 
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
